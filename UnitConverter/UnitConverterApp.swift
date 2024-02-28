eb9de176aa408770585d97f50b28caa154cc861a
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-02-27 18:56:49 -0800

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Arseniy Oksenoyt on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
